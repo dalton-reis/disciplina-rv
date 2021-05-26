8bff3b2cd61970a8f65c4d7f2f373c2ffc67cb9f
dalton-reis <dalton.reis@gmail.com>
2021-05-26 18:40:28 -0300

    Hunter Pro Free

---
+++ Unity/_fontes/RV_PlanoCubo-Mover/Assets/Scripts/Cube.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    // private Transform Plano;
+    private float sinal = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Plano = this.transform.parent;
+        // Debug.Log(Plano.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x >= 4.5) {
+            sinal = -1;
+        }
+        if (transform.position.x <= -4.5) {
+            sinal = 1;
+        }
+        transform.position = transform.position + new Vector3(0.01f*sinal,0,0);
+        Debug.Log(transform.position);
+    }
+}
